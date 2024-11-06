cd2762517074ad25910b4bcbc6360039d9ebf8bd
abhisheksh99 <abhishekleo1234@gmail.com>
2024-11-06 17:28:20 -0500

    Database connected

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,13 @@
-import express from "express"
-import dotenv from "dotenv"
-dotenv.config()
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/DB.js";
+dotenv.config();
 
-const app = express()
+const app = express();
+connectDB();
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 
-app.listen(PORT,()=>{
-    console.log(`Server listening on ${PORT}`);
-    
-})
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
